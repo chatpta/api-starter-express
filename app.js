facd00106f629357909c57f36920ad3f464c7636
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-10-26 15:37:25 -0400

    cors etc added

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const logger = require( 'morgan' );
 const indexRouter = require( './routes/index' );
 const usersRouter = require( './routes/users' );
 const errors = require( "./common/error" );
+const helmet = require( "helmet" );
+const cors = require( "cors" );
 
 const app = express();
 
+app.use( cors() );
+app.use( helmet() );
 app.use( logger( 'dev' ) );
 app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
@@ -18,14 +22,10 @@ app.use( express.static( path.join( __dirname, 'public' ) ) );
 app.use( '/', indexRouter );
 app.use( '/users', usersRouter );
 
-// Not found
-app.use( ( req, res ) => {
-    res.status( 404 ).json( errors.appError404 );
-} );
+// Not found json response
+app.use( ( req, res ) => res.status( 404 ).json( errors.appError404 ) );
 
-// Error handler
-app.use( ( err, req, res, next ) => {
-    res.status(500).json( errors.appError500 );
-} );
+// Server error json response
+app.use( ( err, req, res, next ) => res.status( 500 ).json( errors.appError500 ) );
 
 module.exports = app;
