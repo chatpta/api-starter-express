a6b8db92f793fd29d72aca502df0d55b028b3a40
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-11 14:44:58 -0500

    test error fixed

--- test/app/app.test.js
+++ test/app/app.test.js
@@ -2,13 +2,10 @@ const { describe, it } = require( "mocha" );
 const assert = require( "assert" );
 const request = require( 'supertest' );
 const app = require( '../../app' );
-const commonMiddleware = require( '../../common-middleware' );
+
 
 describe( "Not found app test", function () {
-    it( "returns not found 404 message", function () {
-
-        // noinspection JSCheckFunctionSignatures
-        app.use( commonMiddleware.appErrorHandlers.notFound404 );
+    it( "returns not found 404 message", function ( done ) {
 
         // Act
         request( app )
@@ -19,6 +16,25 @@ describe( "Not found app test", function () {
                 // Assert
                 assert( response.status === 404 );
                 assert( response.body.type === "not found" );
+                done()
             } );
     } );
 } );
+
+describe( "Server error application", function () {
+    it( "returns server error 500 message", function ( done ) {
+
+        // Act
+        request( app )
+            .get( '/error' )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 500 );
+                assert( response.body.type === 'app error' );
+                done();
+            } );
+    } );
+} );
+
