d170870b54f2daaba6f0dfc19762de45d37f13ed
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-11 22:37:05 -0500

    Comments fixed

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const router = express.Router();
 const user = require( '../controller' ).user;
 
-/* GET users listing. */
+
 router.get( '/', user.getRequestHandler );
 router.post( '/', user.postRequestHandler );
 router.patch( '/', user.patchRequestHandler );
