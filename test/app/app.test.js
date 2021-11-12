01af8e64407bdc23557c6a2c026d7523b73aa993
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-11 19:15:34 -0500

    app test

--- test/app/app.test.js
+++ test/app/app.test.js
@@ -4,7 +4,7 @@ const request = require( 'supertest' );
 const app = require( '../../app' );
 
 
-describe( "Not found app", function () {
+describe( "Not found", function () {
     it( "returns not found 404 message", function ( done ) {
 
         // Act
@@ -21,7 +21,7 @@ describe( "Not found app", function () {
     } );
 } );
 
-describe( "Server error app", function () {
+describe( "Server error", function () {
     it( "returns server error 500 message", function ( done ) {
 
         // Act
@@ -33,6 +33,23 @@ describe( "Server error app", function () {
                 // Assert
                 assert( response.status === 500 );
                 assert( response.body.type === 'app error' );
+                done();
+            } );
+    } );
+} );
+
+describe( "Server routes", function () {
+    it( "/get home", function ( done ) {
+
+        // Act
+        request( app )
+            .get( '/' )
+            .end( ( err, response ) => {
+                if ( err ) return;
+
+                // Assert
+                assert( response.status === 200 );
+                assert( response.body.message === 'home get response' );
                 done();
             } );
     } );
