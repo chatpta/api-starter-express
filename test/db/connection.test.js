ffb670e3f9b6e0194c7124cc01aa342c8db2b39b
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-11 22:46:24 -0500

    test with and without database fixed

--- test/db/connection.test.js
+++ test/db/connection.test.js
@@ -2,20 +2,20 @@ const connection = require( '../../db' );
 const { describe, it } = require( "mocha" );
 const assert = require( "assert" );
 
-describe( "Database connection", async function () {
-    it( "connect to database and query to gets time", async function ( done ) {
-        // No testing if no db connection
-        if ( process.env.DB_CONN === "none" ) {
-            done();
-        }
+// Runs test only if DB_CONN is defined
+if ( process.env?.DB_CONN !== "none" ) {
+    describe( "Database connection", async function () {
+        it( "connect to database and query to gets time", async function () {
+            // No testing if no db connection
 
-        // Arrange
-        let time = null;
+            // Arrange
+            let time = null;
 
-        // Act
-        time = await connection.query( 'SELECT NOW()' );
+            // Act
+            time = await connection.query( 'SELECT NOW()' );
 
-        // Assert
-        assert( time );
+            // Assert
+            assert( time.rowCount === 1 );
+        } );
     } );
-} );
+}
