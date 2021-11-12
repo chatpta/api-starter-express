23a076664903f56f86a760e6d315dbc030168ccd
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-11 22:36:39 -0500

    Comments fixed

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const router = express.Router();
 const controller = require( '../controller' );
 
-/* GET home page. */
+
 router.get( '/', controller.home.getRequestHandler );
 router.post( '/', controller.home.postRequestHandler );
 router.patch( '/', controller.home.patchRequestHandler );
