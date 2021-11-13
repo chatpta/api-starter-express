4453a66e053f197b31eaab5c9f3e5a6683d15834
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-13 15:48:22 -0500

    migration added

--- db/migration.js
+++ db/migration.js
@@ -2,6 +2,20 @@
 'use strict';
 
 /**
- * Creates database and tables for starter application
+ * Creates database and tables for starter application.
  */
+
+/**
+ * Load .env file.
+ * Required for database connection.
+ */
+require( 'dotenv' ).config();
+
+/**
+ * Connect to database and test it.
+ */
+const { query, getClient, pool } = require( '../db' );
+query( 'SELECT NOW()' )
+    .then( console.log )
+    .catch( console.log );
 
