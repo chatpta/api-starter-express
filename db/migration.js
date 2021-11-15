e2c3662ffab5e6e646aec21992758d44fdfd4920
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-15 16:48:54 -0500

    User table is created

--- db/migration.js
+++ db/migration.js
@@ -17,10 +17,35 @@ require( 'dotenv' ).config();
 const Factory = require( '../factory' );
 
 Factory.Database.getDbClient()
-    .then( client => client.query( 'SELECT NOW()' ) )
+    .then( client => client.query( `
+          BEGIN;
+               DROP TABLE IF EXISTS Users;
+               CREATE TABLE Users (
+                    user_id SERIAL PRIMARY KEY,
+                    first_name text,
+                    last_name text,
+                    main_body json,
+                    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+               );
+               
+               CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+                    RETURNS TRIGGER AS $$
+                    BEGIN
+                      NEW.updated_at = NOW();
+                      RETURN NEW;
+                    END;
+                    $$ LANGUAGE plpgsql;
+               
+               CREATE TRIGGER set_timestamp
+                    BEFORE UPDATE ON Users
+                    FOR EACH ROW
+                    EXECUTE PROCEDURE trigger_set_timestamp();
+          COMMIT;
+     ` ) )
     .then( console.log )
-    .catch( console.error );
-// .finally( process.exit )
+    .catch( console.error )
+    .finally( process.exit );
 
 // Create database if not exist.
 // Create tables if not exist.
