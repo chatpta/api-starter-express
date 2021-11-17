ee3a45324dfe29aa8413a22f35a48f9f3c5ee255
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-16 20:26:14 -0500

    Active record is working well

--- test/model/item.test.js
+++ test/model/item.test.js
@@ -1,20 +1,66 @@
 const assert = require( 'assert' );
-const { describe, it } = require( "mocha" );
+const { describe, it, beforeEach, afterEach } = require( "mocha" );
 const { Item } = require( "../../factory" );
 
 // Runs test only if DB_CONN is defined
 if ( process.env?.DB_CONN !== "none" ) {
-    // describe( 'Item model', () => {
-    //     it( 'Saves item', ( done ) => {
-    //         // Arrange
-    //         let expectedLine = { record: "Item" };
-    //
-    //         // Act
-    //         let foundLine = Item.save();
-    //
-    //         // Assert
-    //         assert.deepStrictEqual( foundLine, expectedLine );
-    //         done()
-    //     } );
-    // } );
+    describe( 'Item model', () => {
+
+        let testItemId = null;
+
+        beforeEach( async () => {
+            let testItem = await Item.save( { title: "Test Item" } );
+            testItemId = testItem.rows[ 0 ].item_id;
+        } );
+
+        afterEach( async () => {
+            await Item.delete( testItemId );
+        } );
+
+        it( 'Saves item', async () => {
+            // Arrange
+            const testItem = { title: "Save me" };
+
+            // Act
+            let savedItem = await Item.save( testItem );
+            let savedItemTitle = savedItem.rows[ 0 ].title;
+            let savedItemId = savedItem.rows[ 0 ].item_id;
+            await Item.delete( savedItemId );
+
+            // Assert
+            assert.deepStrictEqual( savedItemTitle, testItem.title );
+        } );
+
+
+        it( 'findOne', async () => {
+            // Act
+            const item = await Item.findOne();
+
+            // Assert
+            assert.deepStrictEqual( item.rowCount, 1 );
+        } );
+
+        it( 'findById', async () => {
+            // Act
+            const foundItem = await Item.findById( testItemId );
+            const foundItemId = foundItem.rows[ 0 ].item_id;
+
+            // Assert
+            assert.deepStrictEqual( foundItemId, testItemId );
+        } );
+
+        it( 'update', async () => {
+            // Arrange
+            const updateItem = { title: "Updated item" };
+
+            // Act
+            const updatedItem = await Item.update( testItemId, updateItem );
+            const updateItemId = updatedItem.rows[ 0 ].item_id;
+            const updateItemTitle = updatedItem.rows[ 0 ].title;
+
+            // Assert
+            assert.deepStrictEqual( updateItemId, testItemId );
+            assert.deepStrictEqual( updateItemTitle, "Updated item" );
+        } );
+    } );
 }
