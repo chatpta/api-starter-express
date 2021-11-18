a1572310b3819315d74c70a8f26e58fd273a5053
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-18 10:05:49 -0500

    catch statement shortened

--- controller/usersController.js
+++ controller/usersController.js
@@ -11,26 +11,26 @@ const { User } = require( '../factory' );
 async function getRequestHandler( req, res, next ) {
     await User.findByFirstName( req.query.first_name )
         .then( user => res.send( user.data[ 0 ] ) )
-        .catch( error => next( new Error( error ) ) );
+        .catch( next );
 }
 
 async function postRequestHandler( req, res, next ) {
     await User.save( req.body.user )
         .then( user => res.send( user.data[ 0 ] ) )
-        .catch( error => next( new Error( error ) ) );
+        .catch( next );
 }
 
 async function patchRequestHandler( req, res, next ) {
     await User.findByFirstName( req.body.user.first_name )
         .then( user => User.update( user.data[ 0 ].user_id, req.body.updated_user ) )
         .then( user => res.send( user.data[ 0 ] ) )
-        .catch( error => next( new Error( error ) ) );
+        .catch( next );
 }
 
 async function deleteRequestHandler( req, res, next ) {
     await User.findByFirstName( req.body.user.first_name )
         .then( user => User.delete( user.data[ 0 ].user_id ) )
         .then( user => res.send( user.data[ 0 ] ) )
-        .catch( error => next( new Error( error ) ) );
+        .catch( next );
 }
 
