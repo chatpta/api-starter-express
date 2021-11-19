4a1822a5add10172be4a6f003610e49f51f9e1a7
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-18 21:51:26 -0500

    Optional chaining applied

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -14,32 +14,32 @@ const { Item } = require( '../factory' );
 const lib = require( "./lib/libCommon" );
 
 async function getRequestHandler( req, res, next ) {
-    await Item.findById( req.query.item_id )
+    await Item.findById( req?.query?.item_id )
         .then( item => lib.checkSuccess( item, next ) )
-        .then( item => res.send( item.data[ 0 ] ) )
+        .then( item => res?.send( item?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function postRequestHandler( req, res, next ) {
-    await Item.save( req.body.item )
+    await Item.save( req?.body?.item )
         .then( item => lib.checkSuccess( item, next ) )
-        .then( item => res.send( item.data[ 0 ] ) )
+        .then( item => res?.send( item?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function patchRequestHandler( req, res, next ) {
-    await Item.findById( req.body.item.item_id )
+    await Item.findById( req?.body?.item?.item_id )
         .then( item => lib.checkSuccess( item, next ) )
-        .then( item => Item.update( item.data[ 0 ].item_id, req.body.updated_item ) )
+        .then( item => Item.update( item?.data[ 0 ]?.item_id, req?.body?.updated_item ) )
         .then( item => lib.checkSuccess( item, next ) )
-        .then( item => res.send( item.data[ 0 ] ) )
+        .then( item => res?.send( item?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function deleteRequestHandler( req, res, next ) {
-    await Item.delete( req.body.item.item_id )
+    await Item.delete( req?.body?.item?.item_id )
         .then( item => lib.checkSuccess( item, next ) )
-        .then( item => res.send( item.data[ 0 ] ) )
+        .then( item => res?.send( item?.data[ 0 ] ) )
         .catch( next );
 }
 
