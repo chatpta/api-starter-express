50722d225a16acd086687a698f058a96928b8787
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-18 21:48:38 -0500

    Optional chaining applied

--- controller/usersController.js
+++ controller/usersController.js
@@ -14,33 +14,33 @@ const { User } = require( '../factory' );
 const lib = require( './lib/libCommon' );
 
 async function getRequestHandler( req, res, next ) {
-    await User.findByFirstName( req.query.first_name )
+    await User.findByFirstName( req?.query?.first_name )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => res.send( user.data[ 0 ] ) )
+        .then( user => res?.send( user?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function postRequestHandler( req, res, next ) {
-    await User.save( req.body.user )
+    await User.save( req?.body?.user )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => res.send( user.data[ 0 ] ) )
+        .then( user => res?.send( user?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function patchRequestHandler( req, res, next ) {
-    await User.findByFirstName( req.body.user.first_name )
+    await User.findByFirstName( req?.body?.user?.first_name )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => User.update( user.data[ 0 ].user_id, req.body.updated_user ) )
+        .then( user => User.update( user?.data[ 0 ]?.user_id, req?.body?.updated_user ) )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => res.send( user.data[ 0 ] ) )
+        .then( user => res?.send( user?.data[ 0 ] ) )
         .catch( next );
 }
 
 async function deleteRequestHandler( req, res, next ) {
-    await User.findByFirstName( req.body.user.first_name )
+    await User.findByFirstName( req?.body?.user?.first_name )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => User.delete( user.data[ 0 ].user_id ) )
+        .then( user => User.delete( user?.data[ 0 ]?.user_id ) )
         .then( user => lib.checkSuccess( user, next ) )
-        .then( user => res.send( user.data[ 0 ] ) )
+        .then( user => res?.send( user?.data[ 0 ] ) )
         .catch( next );
 }
