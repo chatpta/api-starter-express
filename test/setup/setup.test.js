f7ae343c84b92e99963d52e162da3ec937f9d2c6
PK Singh <pksin@ulaval.ca>
2021-11-22 06:24:48 -0500

    two test users are added in test setup

--- test/setup/setup.test.js
+++ test/setup/setup.test.js
@@ -1,14 +1,28 @@
 'use strict';
 const { before, after } = require( "mocha" );
 const db = require( '../../db' );
+const { User } = require( "../../factory" );
+
+let testUserIdSetup = null;
+let testUser2IdSetup = null;
 
 before( "Setup test environment before all tests", async function () {
     // Load .env file to process.env
     require( 'dotenv' ).config();
+
+    let testUser = await User.save( { first_name: "Test User setup one" } );
+    testUserIdSetup = testUser.data[ 0 ].user_id;
+
+    let testUser2 = await User.save( { first_name: "Test User setup two" } );
+    testUser2IdSetup = testUser2.data[ 0 ].user_id;
 } );
 
-after( "Disconnect pool", function () {
-    db.getEndPool()
+after( "Disconnect pool", async function () {
+
+    await User.delete( testUserIdSetup );
+    await User.delete( testUser2IdSetup );
+
+    await db.getEndPool()
         .then( console.log )
         .catch( console.error );
 } );
