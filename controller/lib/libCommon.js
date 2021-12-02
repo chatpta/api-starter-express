8615e305a3c006f4c1d6be60e987d3bc93c1986a
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-02 16:11:14 -0500

    new validation function added

--- controller/lib/libCommon.js
+++ controller/lib/libCommon.js
@@ -4,13 +4,15 @@
  * This file contains functions common to all controllers.
  */
 
+const validate = require( "@chatpta/validate" );
+
 /**
  * Skips the route if does not receive data in dto
  * @param dto
  * @param next
  * @return {{success}|*}
  */
-function checkSuccess( dto, next ) {
+function _checkSuccess( dto, next ) {
     if ( dto?.success ) {
         return dto;
     } else {
@@ -18,4 +20,53 @@ function checkSuccess( dto, next ) {
     }
 }
 
-module.exports = { checkSuccess };
+function _verifyObjectProperties( obj, arrayOfProperties ) {
+    let returnObj = {};
+
+    arrayOfProperties.forEach( nameOfProperty => {
+        if ( obj.hasOwnProperty( nameOfProperty ) ) {
+            returnObj[ nameOfProperty ] = obj[ nameOfProperty ];
+        }
+    } );
+
+    return returnObj;
+}
+
+function _validateProperties( obj ) {
+    let returnObj = {};
+
+    for ( let [ key, value ] of Object.entries( obj ) ) {
+
+        switch ( key ) {
+            case "first_name" :
+            case "last_name" :
+                validate.isCharactersString( value ) ? returnObj[ key ] = value : null;
+                break;
+            case "email" :
+                validate.isEmailString( value ) ? returnObj[ key ] = value : null;
+                break;
+            case "phone_number":
+                _isPhoneNumber( value ) ? returnObj[ key ] = value : null;
+                break;
+        }
+    }
+
+    return returnObj;
+}
+
+function _isPhoneNumber( str ) {
+    if ( str === undefined || typeof str !== "string" || str.length === 0 ) return false;
+    return ( /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test( str ) );
+}
+
+function _throwValidationFailureError() {
+    throw new Error( "Validation_Failure" );
+}
+
+module.exports = {
+    checkSuccess: _checkSuccess,
+    _verifyObjectProperties,
+    _validateProperties,
+    _isPhoneNumber,
+    throwValidationFailureError: _throwValidationFailureError
+};
